51dbb823ba3c153b2763a77c0f87919e06a358e6
motovmp1 <vinicius.mpinho@gmail.com>
2020-07-15 08:55:40 +0800

    Running test 10

---
+++ src/Jump_example/Jump_001.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package Jump_example;
+
+/**
+ * @author elsys
+ *
+ */
+public class Jump_001 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i =1; i<=10; i++) {
+			
+			if (i==6) 
+			{
+				break;
+			}
+			
+			System.out.println(i);
+		}
+		
+
+	}
+
+}
